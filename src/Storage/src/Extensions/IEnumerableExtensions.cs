b406290c0ff8cf0e7dbe9d296f6e41bcd3f3539c
Roman St <fcromich@gmail.com>
2024-04-17 01:49:15 +0200

    minor fix

--- src/Storage/src/Extensions/IEnumerableExtensions.cs
+++ src/Storage/src/Extensions/IEnumerableExtensions.cs
@@ -6,24 +6,23 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace IdentityServer4.Extensions
+namespace IdentityServer4.Extensions;
+
+internal static class IEnumerableExtensions
 {
-    internal static class IEnumerableExtensions
+    [DebuggerStepThrough]
+    public static bool IsNullOrEmpty<T>(this IEnumerable<T> list)
     {
-        [DebuggerStepThrough]
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> list)
+        if (list == null)
         {
-            if (list == null)
-            {
-                return true;
-            }
-
-            if (!list.Any())
-            {
-                return true;
-            }
-
-            return false;
+            return true;
         }
+
+        if (!list.Any())
+        {
+            return true;
+        }
+
+        return false;
     }
 }
