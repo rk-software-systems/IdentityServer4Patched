135cb715cd622cd5441870cbd84079cc6f5d1bcf
Roman St <fcromich@gmail.com>
2024-04-17 03:10:28 +0200

    update pipeline

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ mkdir nuget
 
 dotnet tool restore
 
-pushd ./src/Storage
-./build.sh "$@"
-popd
+#pushd ./src/Storage
+#./build.sh "$@"
+#popd
 
 #pushd ./src/IdentityServer4
 #./build.sh "$@"
 #popd
 
-#pushd ./src/EntityFramework.Storage
-#./build.sh "$@"
-#popd
+pushd ./src/EntityFramework.Storage
+./build.sh "$@"
+popd
 
 #pushd ./src/EntityFramework
 #./build.sh "$@"
