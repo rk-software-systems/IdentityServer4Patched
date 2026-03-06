18586cea5a060341691aae0409b27d357c3c7fbf
Roman St <fcromich@gmail.com>
2026-03-07 00:33:40 +0100

    feat: fix tests

--- src/EntityFramework/test/IdentityServer4.EntityFramework.Tests/Services/CorsPolicyServiceTests.cs
+++ src/EntityFramework/test/IdentityServer4.EntityFramework.Tests/Services/CorsPolicyServiceTests.cs
@@ -22,7 +22,7 @@ namespace IdentityServer4.EntityFramework.IntegrationTests.Services
     {
         public CorsPolicyServiceTests(DatabaseProviderFixture<ConfigurationDbContext> fixture) : base(fixture)
         {
-            foreach (var options in TestDatabaseProviders.SelectMany(x => x.Select(y => (DbContextOptions<ConfigurationDbContext>)y)).ToList())
+            foreach (var options in ((IEnumerable<object[]>)TestDatabaseProviders).SelectMany(x => x.Select(y => (DbContextOptions<ConfigurationDbContext>)y)).ToList())
             {
                 using (var context = new ConfigurationDbContext(options, StoreOptions))
                     context.Database.EnsureCreated();
